ca265cfe679411fe255c772b863a72b72a6b5afc
Lauren Days <lmchambers0420@gmail.com>
2018-09-14 20:30:01 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,13 +37,5 @@ end
 
 # Define your play method below
 def play(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
-  turn(board)
+  turn(board
 end
